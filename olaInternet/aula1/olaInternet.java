e58729666b228e897280db300f10d1a23297cd8e
shizuo-hachi <ashqdr@gmail.com>
2023-11-09 16:05:02 -0300

    t

---
+++ olaInternet/aula1/olaInternet.java
@@ -0,0 +1,5 @@
+public class olaInternet{
+  public static void main(String [] args){
+    System.out.println("Olá, Shizuo!"); 
+  }
+}
---
+++ olaInternet/aula2/olaInternet.java
@@ -0,0 +1,14 @@
+public class olaInternet{
+    //Classe de demonstração para o curso do desenquadros
+  public static void main(String [] args){
+      //imprime algo na tela
+    System.out.println("Olá, Shizuo!"); // comentário no final
+    int _34123 = 1;
+    System.out.println(556);
+  }
+  /*
+   *Isso é um comentário
+   *em muitas 
+   *linhas de texto
+   */
+}
